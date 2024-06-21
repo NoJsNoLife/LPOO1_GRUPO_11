dad2e56efe8953a9ef3a293636578e5375a3276a
Darío Rubén Vidal <96150592+DarioRv@users.noreply.github.com>
2024-06-21 14:52:56 -0300

    Añadir abm de disciplinas

---
+++ ClaseBase/TrabajarDisciplina.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseBase
+{
+    public class TrabajarDisciplina
+    {
+        public static bool existeById(int id)
+        {
+            string conexion = DataBaseConfig.DB_CONN;
+            SqlConnection cnn = new SqlConnection(conexion);
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandText = "SELECT * FROM Atleta WHERE Dis_ID = @id";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cnn;
+
+            cmd.Parameters.AddWithValue("@id", id);
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            if (dt.Rows.Count == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static bool existeByNombre(string nombre)
+        {
+            string conexion = DataBaseConfig.DB_CONN;
+            SqlConnection cnn = new SqlConnection(conexion);
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandText = "SELECT * FROM Disciplina WHERE Dis_Nombre = @nombre";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cnn;
+
+            cmd.Parameters.AddWithValue("@nombre", nombre);
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            if (dt.Rows.Count == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static bool existeNombreDuplicado(string nombre, int id)
+        {
+            string conexion = DataBaseConfig.DB_CONN;
+            SqlConnection cnn = new SqlConnection(conexion);
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandText = "SELECT * FROM Disciplina WHERE Dis_Nombre = @nombre AND Dis_ID != @id";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cnn;
+
+            cmd.Parameters.AddWithValue("@nombre", nombre);
+            cmd.Parameters.AddWithValue("@id", id);
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            if (dt.Rows.Count == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public static void alta_disciplina(string nombre, string descripcion)
+        {
+            string conexion = DataBaseConfig.DB_CONN;
+            SqlConnection cnn = new SqlConnection(conexion);
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandText = "INSERT INTO Disciplina " +
+                "(Dis_Nombre, Dis_Descripcion) " +
+                "VALUES (@nombre, @descripcion)";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cnn;
+            cmd.Parameters.AddWithValue("@nombre", nombre);
+            cmd.Parameters.AddWithValue("@descripcion", descripcion);
+
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+        }
+
+        public static void baja_disciplina(int id)
+        {
+            string conexion = DataBaseConfig.DB_CONN;
+            SqlConnection cnn = new SqlConnection(conexion);
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandText = "DELETE FROM Disciplina WHERE Dis_ID = @id";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cnn;
+            cmd.Parameters.AddWithValue("@id", id);
+
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+        }
+
+        public static void modificar_disciplina(int id, string nombre, string descripcion)
+        {
+            string conexion = DataBaseConfig.DB_CONN;
+            SqlConnection cnn = new SqlConnection(conexion);
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandText = "UPDATE Disciplina SET Dis_Nombre = @nombre, Dis_Descripcion = @descripcion WHERE Dis_ID = @id";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cnn;
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.Parameters.AddWithValue("@nombre", nombre);
+            cmd.Parameters.AddWithValue("@descripcion", descripcion);
+
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+        }
+
+        public static DataTable listar_disciplinas()
+        {
+            string conexion = DataBaseConfig.DB_CONN;
+            SqlConnection cnn = new SqlConnection(conexion);
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandText = "SELECT Dis_Id as ID, Dis_Nombre as Nombre, Dis_Descripcion as Descripcion FROM Disciplina";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cnn;
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            return dt;
+        }
+    }
+}
---
+++ Vistas/ABMDisciplinas.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClaseBase;
+using Microsoft.VisualBasic;
+
+namespace Vistas
+{
+    public partial class ABMDisciplinas : Form
+    {
+        private Form referencia;
+        private int disciplinaId = 0;
+        public ABMDisciplinas(Form referencia)
+        {
+            InitializeComponent();
+            this.referencia = referencia;
+        }
+
+        private void AltaDisciplina_Load(object sender, EventArgs e)
+        {
+            load_disciplinas();
+        }
+
+        private void load_disciplinas()
+        {
+            dgwDisciplinas.DataSource = TrabajarDisciplina.listar_disciplinas();
+            dgwDisciplinas.Columns["ID"].Visible = false;
+        }
+
+        private void btnAgregarDisciplina_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form altaDisciplina = new AltaDisciplina(this);
+            altaDisciplina.Show();
+        }
+
+        private void ABMDisciplinas_Load(object sender, EventArgs e)
+        {
+            load_disciplinas();
+        }
+
+        private void btnVolverSistema_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            this.referencia.Show();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            if (disciplinaId != 0)
+            {
+                string nuevoNombre = txtNombreDis.Text;
+                string nuevaDescripcion = txtDescripcionDis.Text;
+                if (TrabajarDisciplina.existeNombreDuplicado(nuevoNombre, disciplinaId))
+                {
+                    MessageBox.Show("Esa disciplina ya existe");
+                    return;
+                }
+                else
+                {
+                    TrabajarDisciplina.modificar_disciplina(disciplinaId, nuevoNombre, nuevaDescripcion);
+                    load_disciplinas();
+                    MessageBox.Show("Disciplina modificada con éxito");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Debe seleccionar una disciplina para modificar");
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (disciplinaId != 0)
+            {
+                DialogResult dialogResult = MessageBox.Show("¿Está seguro que desea eliminar la disciplina?", "Eliminar disciplina", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    TrabajarDisciplina.baja_disciplina(disciplinaId);
+                    load_disciplinas();
+                    MessageBox.Show("Disciplina eliminada con éxito");
+                    limpiarVariables();
+                }
+            }
+        }
+
+        private void limpiarVariables()
+        {
+            txtDescripcionDis.Text = "";
+            txtNombreDis.Text = "";
+            disciplinaId = 0;
+        }
+
+        private void dgwDisciplinas_CurrentCellChanged(object sender, EventArgs e)
+        {
+            if (dgwDisciplinas.CurrentRow != null && !Convert.IsDBNull(dgwDisciplinas.CurrentRow.Cells["ID"].Value))
+            {
+                disciplinaId = Convert.ToInt32(dgwDisciplinas.CurrentRow.Cells["ID"].Value);
+                txtNombreDis.Text = dgwDisciplinas.CurrentRow.Cells["Nombre"].Value.ToString();
+                txtDescripcionDis.Text = dgwDisciplinas.CurrentRow.Cells["Descripcion"].Value.ToString();
+            }
+        }
+    }
+}
